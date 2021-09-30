98397d165d8d520484226914611c85700402b8ba
vishwapandya <57280185+vishwapandya@users.noreply.github.com>
2021-09-30 10:22:17 +0530

    Add files via upload

---
+++ trial.py
@@ -0,0 +1,39 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+
+class App(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 button'
+        self.left = 10
+        self.top = 10
+        self.width = 320
+        self.height = 200
+        self.initUI()
+    
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.textBox = QLineEdit(self)
+        self.textBox.move(20,80)
+        self.textBox.resize(200,40)
+        self.button = QPushButton('Show Text', self)
+        self.button.move(20,120)
+        self.button.clicked.connect(self.on_click)
+        
+        self.show()
+
+    @pyqtSlot()
+    def on_click(self):
+        # print('PyQt5 button click')
+        textBoxValue = self.textBox.text()
+        QMessageBox.question(self,"Message","You Typed"+textBoxValue,QMessageBox.Ok,QMessageBox.Ok)
+        self.textBox.setText("")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
